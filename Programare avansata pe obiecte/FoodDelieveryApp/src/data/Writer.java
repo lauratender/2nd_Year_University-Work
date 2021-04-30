fe1bbec2e0be1278226aba8ed30bb00ba318d7bb
lauratender <62187642+lauratender@users.noreply.github.com>
2021-04-30 18:23:01 +0300

    Wrote csv files

---
+++ Programare avansata pe obiecte/FoodDelieveryApp/src/data/Writer.java
@@ -0,0 +1,39 @@
+package data;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writer {
+    private static final String COMMA_DELIMITER = ",";
+    private static Writer instance = null;
+
+    private Writer(){}
+
+    public void writeLine(String filename, String line) {
+        try {
+            String path = "src/data/csv/" + filename + ".csv";
+            File file = new File(path);
+            FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(line);
+            bw.close();
+
+           /*
+            FileWriter csvWriter = new FileWriter(path);
+            csvWriter.append(line);
+            csvWriter.flush();
+            csvWriter.close();
+            */
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Writer getInstance(){
+        if (instance == null)
+            instance = new Writer();
+        return instance;
+    }
+}
